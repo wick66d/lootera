2f14ccef33dce22585976eb5336a38510f3034ac
wick66d <gustasgre@gmail.com>
2025-11-22 00:13:10 +0200

    Pagination, basic search

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,26 +1,22 @@
+using API.RequestHelpers;
 using Core.Entities;
 using Core.Interfaces;
 using Core.Specifications;
-using Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
-[ApiController]
-[Route("api/[controller]")]
-public class ProductsController(IGenericRepository<Product> repo) : ControllerBase
+public class ProductsController(IGenericRepository<Product> repo) : BaseApiController
 {
 
     [HttpGet]
-    public async Task<ActionResult<IEnumerable<Product>>> GetProducts(string? brand, 
-        string? type, string? sort)
+    public async Task<ActionResult<IEnumerable<Product>>> GetProducts(
+        [FromQuery]ProductSpecParams specParams)
     {
-        var spec = new ProductSpecification(brand, type, sort);
+        System.Console.WriteLine($"Search term in controller : {specParams.Search}");
+        var spec = new ProductSpecification(specParams);
 
-        var products = await repo.ListAsync(spec);
-
-        return Ok(products);
+        return await CreatePagedResult(repo, spec, specParams.PageIndex, specParams.PageSize);
     }
 
     [HttpGet("{id:int}")] //api/products/2
