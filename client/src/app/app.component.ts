cd8369a78a5f3ab7c2243c72a3934ac9998587c0
wick66d <gustasgre@gmail.com>
2025-11-23 14:45:50 +0200

    Angular setup, header component

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './layout/header/header.component';
+import { HttpClient } from '@angular/common/http';
+import { Product } from './shared/models/product';
+import { Pagination } from './shared/models/pagination';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, HeaderComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+})
+export class AppComponent implements OnInit {
+  baseUrl = 'https://localhost:5001/api/';
+  private http = inject(HttpClient);
+  protected readonly title = 'client';
+  products: Product[] = [];
+
+  ngOnInit(): void {
+    this.http.get<Pagination<Product>>(this.baseUrl + 'products').subscribe({
+      next: response => this.products = response.data,
+      error: (error) => console.log(error),
+      complete: () => console.log('complete'),
+    });
+  }
+}
