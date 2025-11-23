1be317e9dec908480b590d11259b97e1d11f1e32
wick66d <gustasgre@gmail.com>
2025-11-24 01:20:25 +0200

    Adding the filtering functionality using Material Dialog(no hookup to service yet)

--- client/src/app/features/shop/shop.component.ts
+++ client/src/app/features/shop/shop.component.ts
@@ -1,22 +1,58 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { Product } from '../../shared/models/product';
 import { ShopService } from '../../core/services/shop.service';
-import { MatCard } from '@angular/material/card';
+import { MatDialog } from '@angular/material/dialog';
 import { ProductItemComponent } from './product-item/product-item.component';
+import { FiltersDialogComponent } from './filters-dialog/filters-dialog.component';
+import { MatButton } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-shop',
-  imports: [ProductItemComponent],
+  imports: [ProductItemComponent, MatButton, MatIcon],
   templateUrl: './shop.component.html',
   styleUrl: './shop.component.scss',
 })
 export class ShopComponent implements OnInit {
   private shopService = inject(ShopService);
+  private dialogService = inject(MatDialog);
   products: Product[] = [];
+  selectedBrands: string[] = [];
+  selectedTypes: string[] = [];
   ngOnInit(): void {
+    this.initializeShop();
+  }
+
+  initializeShop() {
+    this.shopService.getBrands();
+    this.shopService.getTypes();
     this.shopService.getProducts().subscribe({
       next: (response) => (this.products = response.data),
       error: (error) => console.log(error),
+    });
+    this.shopService.getProducts().subscribe({
+      next: (response) => (this.products = response.data),
+      error: (error) => console.log(error),
+    });
+  }
+
+  openFiltersDialog() {
+    const dialogRef = this.dialogService.open(FiltersDialogComponent, {
+      minWidth: '500px',
+      data: {
+        selectedBrands: this.selectedBrands,
+        selectedTypes: this.selectedTypes,
+      },
+    });
+    dialogRef.afterClosed().subscribe({
+      next: (result) => {
+        if (result) {
+          console.log(result);
+          this.selectedBrands = result.selectedBrands;
+          this.selectedTypes = result.selectedTypes;
+          //apply filters
+        }
+      },
     });
   }
 }
